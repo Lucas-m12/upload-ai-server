a80375167abd83827822c83d176a98ead9a6d6b9
lucas-m12 <lucasmarinhodasilva2@gmail.com>
2023-09-12 21:26:56 -0300

    first commit

---
+++ src/server.ts
@@ -0,0 +1,13 @@
+import { fastify } from 'fastify';
+
+const PORT = 3333;
+
+const app = fastify();
+app.get('/', () => {
+	return 'Hello World';
+});
+app.listen({
+	port: PORT,
+}).then(() => {
+	console.log('Server running at port', PORT);
+});
